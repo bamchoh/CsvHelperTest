2854384b8314510cca9c33446570d1d1284023db
Yoshihiko Yamanaka <bamchoh@gmail.com>
2022-10-01 01:49:48 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp34/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CsvHelper.Configuration.Attributes;
+
+namespace ConsoleApp34
+{
+    public class Person
+    {
+        [Index(0)]
+        public int Id { get; set; }
+
+        [Index(1)]
+        public string Name { get; set; }
+
+        [Index(2)]
+        public int Age { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+    }
+}
